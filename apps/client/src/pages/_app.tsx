3a80e1a4407f159ccfb91332d3e07e1e7d6d43ba
Piotr Wierzbicki <piotr.w@masterborn.com>
2022-07-06 13:53:12 +0200

    fix: merge related fixes

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,9 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { Button } from 'ui';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div><Button primary label="Button label" /></div>
+  return (
+    <div>
+      <Button primary label="Button label" />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
