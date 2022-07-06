9933016c1dc8aab7b36742bcccf1b7785c059a65
pawk3k <ravliv.pavlobohdan@gmail.com>
2022-07-06 11:59:59 +0200

    build: add web-vitals

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -2,6 +2,7 @@
 /* eslint-disable import/extensions */
 module.exports = {
   ...require('eslint-config-custom/eslintrc-frontend.js'),
+  extends: ['plugin:@next/next/core-web-vitals'],
   parserOptions: {
     project: 'tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
