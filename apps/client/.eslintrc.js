e5c0c7df0e4199c2f503bed3c0b3dace2ba323e8
Piotr Wierzbicki <piotr.w@masterborn.com>
2022-07-07 14:17:20 +0200

    refactor: rename config package, set up fresh eslint config

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,8 +1,6 @@
-/* eslint-disable global-require */
-/* eslint-disable import/extensions */
 module.exports = {
-  ...require('eslint-config-custom/eslintrc-frontend.js'),
-  extends: ['plugin:@next/next/core-web-vitals'],
+  ...require('config/eslintrc-client.js'),
+  // extends: ['plugin:@next/next/core-web-vitals'],
   parserOptions: {
     project: 'tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,12 +1,9 @@
-import type { AppProps } from 'next/app';
 import { Button } from 'ui';
 
-export const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return (
-    <div>
-      <Button primary label="Button label" />
-    </div>
-  );
-};
+export const MyApp = (): JSX.Element => (
+  <div>
+    <Button primary label="Button label" />
+  </div>
+);
 
 export default MyApp;
