96baac91bf40e65a1f55ed2f29d117cd565ccdb2
pawk3k <ravliv.pavlobohdan@gmail.com>
2022-07-07 11:54:05 +0200

    fix: adjust eslint config

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,5 +1,3 @@
-/* eslint-disable global-require */
-/* eslint-disable import/extensions */
 module.exports = {
   ...require('eslint-config-custom/eslintrc-frontend.js'),
   extends: ['plugin:@next/next/core-web-vitals'],
