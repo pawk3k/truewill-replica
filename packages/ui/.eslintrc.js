474935c7c0a7374f30b5a8770476c2c6d360d5e6
Piotr Wierzbicki <piotr.w@masterborn.com>
2022-07-07 14:21:33 +0200

    chore: reproduce changes from staging

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   ...require('config/eslintrc-client.js'),
-  // extends: ['plugin:@next/next/core-web-vitals'],
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
